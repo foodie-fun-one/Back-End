ef07155adb11e4d7f7bd2c3e69e8fbb118973273
lukasjaronis <jaronisluke13@gmail.com>
2020-02-01 18:39:13 -0500

    added server.js

---
+++ api/server.js
@@ -0,0 +1,18 @@
+// imports
+
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cros');
+
+// creating express server
+const server = express();
+
+// importing routers here
+
+// importing middleware here
+
+// middleware server.use here
+
+server.get('/', (req, res) => {
+    res.send(<h1>SERVER IS LIVE</h1>)
+})
