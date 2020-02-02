4ae24bb998e27a549116facd9f335be982f6346f
lukasjaronis <jaronisluke13@gmail.com>
2020-02-01 19:44:57 -0500

    .

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const server = require('./api/server');
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 8015;
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`)
