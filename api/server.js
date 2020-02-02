6e15738e29e0b0a08d393e0bb68045446f7ab98d
lukasjaronis <jaronisluke13@gmail.com>
2020-02-01 19:49:03 -0500

    updated spelling mistake

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cros');
+const cors = require('cors');
 
 // creating express server
 const server = express();
