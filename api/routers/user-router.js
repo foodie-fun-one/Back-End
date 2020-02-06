748892291de7043eb8ab08d9c16361be6bab3b74
lukasjaronis <jaronisluke13@gmail.com>
2020-02-06 13:17:20 -0500

    updated router

--- api/routers/user-router.js
+++ api/routers/user-router.js
@@ -43,7 +43,8 @@ router.post("/login", validateLogin, (req, res) => {
   const token = generateToken(req.body);
   res.status(200).json({
     welcomeMessage: `Logged in as (${req.req_id}) ${req.body.username}.`,
-    token: token
+    token: token,
+    username
   });
 });
 
